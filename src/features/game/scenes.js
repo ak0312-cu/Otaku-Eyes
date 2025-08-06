1980b678d582d8220ef72c648e14ccdf3b52a677
ak0312-cu <akar2003.rg@gmail.com>
2025-08-07 00:03:01 +0530

    changes

--- src/features/game/scenes.js
+++ src/features/game/scenes.js
@@ -1,22 +1,46 @@
 import narutoImg from '../../assets/scenes/naruto.jpg';
 import deathnoteImg from '../../assets/scenes/deathnote.jpg';
 import aotImg from '../../assets/scenes/attackontitan.jpg';
+import bluelockImg from '../../assets/scenes/bluelock.jpg';
+import haikyuuImg from '../../assets/scenes/haikyuu.jpg';
+import jjkImg from '../../assets/scenes/jjk.jpg';
+import yournameImg from '../../assets/scenes/yourname.jpg';
 
 const scenes = [
   {
     id: 1,
     image: narutoImg,
-    answers: ['Naruto', 'naruto', 'NARUTO', 'ナルト'],
+    answers: ['Naruto', 'ナルト'],
   },
   {
     id: 2,
     image: deathnoteImg,
-    answers: ['Death Note', 'DeathNote', 'Death note', 'DEATH NOTE', 'death note', 'デスノート'],
+    answers: ['Death Note', 'DeathNote', 'デスノート'],
   },
   {
     id: 3,
     image: aotImg,
-    answers: ['Attack on Titan', 'Attack On Titan', 'ATTACK ON TITAN', 'AOT', 'Aot', 'aot', 'Shingeki no Kyojin', '進撃の巨人'],
+    answers: ['Attack on Titan', 'AOT', 'Shingeki no Kyojin', '進撃の巨人'],
+  },
+  {
+    id: 4,
+    image: bluelockImg,
+    answers: ['Blue Lock', 'BlueLock', 'ブルーロック'],
+  },
+  {
+    id: 5,
+    image: haikyuuImg,
+    answers: ['haikyuu', 'ハイキュー'],
+  },
+  {
+    id: 6,
+    image: jjkImg,
+    answers: ['jujutsu kaisen', 'jjk', '呪術廻戦'],
+  },
+  {
+    id: 7,
+    image: yournameImg,
+    answers: ['your name', 'yourname', 'Kimi no Na wa', '君の名は'],
   },
 ];
 
